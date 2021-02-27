43fa36b680629b24eef9f91cad9ec2b80445cf7f
NowoCodes <toluwaniokunowo@gmail.com>
2021-02-27 13:51:08 +0100

    Add Values

--- index.php
+++ index.php
@@ -19,40 +19,46 @@
         <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
           <div class="row">
             <div class="form-group col-md-6 mb-2">
-              <input type="text" class="form-control" placeholder="Firstname" />
+              <input type="text" class="form-control" placeholder="Firstname" value="<?= $firstname; ?>" />
             </div>
             <div class="form-group col-md-6 mb-2">
-              <input type="text" class="form-control" name="lastname" placeholder="Lastname" />
+              <input type="text" class="form-control" name="lastname" placeholder="Lastname" value="<?= $lastname; ?>" />
             </div>
           </div>
 
           <div class="row">
             <div class="form-group col-md-6 mb-2">
-              <input type="text" class="form-control" placeholder="Phone No" />
+              <input type="text" class="form-control" placeholder="Phone No" value="<?= $number; ?>" />
             </div>
             <div class="form-group col-md-6 mb-2">
-              <input type="email" class="form-control" name="email" placeholder="Email" />
+              <input type="email" class="form-control" name="email" placeholder="Email" value="<?= $email; ?>" />
             </div>
           </div>
 
           <div class="row">
             <div class="col-6 form-group">
-              <label for="gender">Gender: </label>
-              <input type="radio" class="" name="gender" />
+              <label for="">Gender: </label>
+              <input type="radio" class="" name="gender" value="male" <?php isset($gender) && $gender == "male" ? "checked" : ""; ?>/> Male
+              <input type="radio" class="" name="gender" value="female" <?php isset($gender) && $gender == "female" ? "checked" : ""; ?>/> Female
             </div>
             <div class="col-6 form-group">
               <label for="age">Age: </label>
-              <select name="age" >
+              <select name="age">
                 <option value="">Select One</option>
                 <?php for ($i=1; $i<=20; $i++): ?>
-                <option value="<?= $i; ?>"><?= $i; ?></option>
+                <option <?php isset($age) && $age == $i ? "selected" : ""; ?> value="<?= $i; ?>"><?= $i; ?></option>
                 <?php endfor; ?>
               </select>
             </div>
           </div>
+
+          <div class="d-grid">
+            <button class="btn btn-primary mt-2" type="submit">Submit</button>
+          </div>
         </form>
       </div>
-    </div>
+
+      
   </main>
 
   <script src="dist/js/bootstrap.js"></script>
