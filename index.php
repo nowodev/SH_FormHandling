78b1761be01318fdc6d6e3334273971f51ca844b
NowoCodes <toluwaniokunowo@gmail.com>
2021-02-27 14:24:27 +0100

    Formatted Code

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
-<?php 
+<?php
+function sanitize($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
-  function sanitize($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
+$firstname = $lastname = $number = $email = "";
+$gender = $age = "";
 
-  $firstname = $lastname = $number = $email = "";
-  $gender = $age = "";
-
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $firstname = sanitize($_POST['firstname']);
-    $lastname = sanitize($_POST['lastname']);
-    $number = sanitize($_POST['number']);
-    $email = sanitize($_POST['email']);
-    $gender = sanitize($_POST['gender']);
-    $age = sanitize($_POST['age']);
-  }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $firstname = sanitize($_POST['firstname']);
+  $lastname = sanitize($_POST['lastname']);
+  $number = sanitize($_POST['number']);
+  $email = sanitize($_POST['email']);
+  $gender = sanitize($_POST['gender']);
+  $age = sanitize($_POST['age']);
+}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -34,12 +33,12 @@
 
 <body>
   <main class="container pt-5">
-    <h1 class="text-center font-weight-bold">Form Validation</h1>
+    <h1 class="text-center fw-bold">Form Validation</h1>
 
     <div class="card">
       <div class="card-header"></div>
       <div class="card-body">
-        <form action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+        <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
           <div class="row">
             <div class="form-group col-md-6 mb-2">
               <input type="text" class="form-control" name="firstname" placeholder="Firstname" value="<?= $firstname; ?>" />
@@ -61,15 +60,15 @@
           <div class="row">
             <div class="col-6 form-group">
               <label for="">Gender: </label>
-              <input type="radio" name="gender" value="Male" <?php isset($gender) && $gender == "male" ? "checked" : ""; ?>/> Male
-              <input type="radio" name="gender" value="Female" <?php isset($gender) && $gender == "female" ? "checked" : ""; ?>/> Female
+              <input type="radio" name="gender" value="Male" <?= isset($gender) && $gender == "Male" ? "checked" : ""; ?> /> Male
+              <input type="radio" name="gender" value="Female" <?= isset($gender) && $gender == "Female" ? "checked" : ""; ?> /> Female
             </div>
             <div class="col-6 form-group">
               <label for="age">Age: </label>
               <select name="age">
                 <option value="">Select One</option>
-                <?php for ($i=1; $i<=20; $i++): ?>
-                <option <?php isset($age) && $age == $i ? "selected" : ""; ?> value="<?= $i; ?>"><?= $i; ?></option>
+                <?php for ($i = 1; $i <= 20; $i++) : ?>
+                  <option value="<?= $i; ?>" <?= isset($age) && $age == $i ? "selected" : ""; ?>><?= $i; ?></option>
                 <?php endfor; ?>
               </select>
             </div>
@@ -83,21 +82,17 @@
 
 
       <div class="card-footer">
-      <?php
-        echo "<h2>Your Input:</h2>";
-        echo $firstname . ' ' . $lastname;
-        echo "<br>";
-        echo $number;
-        echo "<br>";
-        echo $email;
-        echo "<br>";
-        echo $gender;
-        echo "<br>";
-        echo $age;
+        <?=
+         '<h2 class="fw-bold">Result:</h2>',
+         $firstname . ' ' . $lastname . '<br>',
+         $number . '<br>',
+         $email . '<br>',
+         $gender . '<br>',
+         $age;
         ?>
       </div>
     </div>
-      
+
   </main>
 
   <script src="dist/js/bootstrap.js"></script>
